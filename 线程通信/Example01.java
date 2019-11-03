a92130dc809218afcb923acd4cb07366ebf7b44c
Xuning998 <55123483+Xuning998@users.noreply.github.com>
2019-11-03 22:53:52 +0800

    第六次作业1

---
+++ 线程通信/Example01.java
@@ -0,0 +1,70 @@
+class Storage
+{
+	private int[] cells=new int[10];
+	private int inpos,outpos;
+	private int count;
+	public synchronized void put(int num){
+		try{
+		while(count==cells.length)
+			this.wait();
+	cells[inpos]=num;
+	System.out.println("在cells["+inpos+"]中放入数据----"+cells[inpos]);
+    inpos++;
+	if(inpos==cells.length)
+		inpos=0;
+	count++;
+      this.notify();
+	}catch(Exception e){
+	e.printStackTrace();
+	}
+	}
+	public synchronized void get(){
+		try{
+			while(count==0)
+				this.wait();
+	int data=cells[outpos];
+	System.out.println("在cells["+outpos+"中取出数据----"+cells[outpos]);
+	outpos++;
+	if(outpos==cells.length)
+		outpos=0;
+	count--;
+	this.notify();
+	}catch(Exception e){
+	e.printStackTrace();
+	}
+	}
+}
+class Input implements Runnable
+{
+	private Storage st;
+	private int num;
+	Input(Storage st){
+	this.st=st;
+	}
+	public void run(){
+	while(true){
+	st.put(num++);}
+	}
+}
+class Output implements Runnable
+{
+	private Storage st;
+	Output(Storage st){
+	this.st=st;
+	}
+	public void run(){
+	while(true){
+	st.get();
+	}
+	}
+}
+public class Example01
+{
+	public static void main(String[] args){
+	Storage st=new Storage();
+	Output output=new Output(st);
+	Input input=new Input(st);
+	new Thread(input).start();
+	new Thread(output).start();
+}
+} 
